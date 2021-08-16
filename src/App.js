daecd0a93923f745da4cdc1d0270d63bf57be44e
lrnzmd <lrnzmd@gmail.com>
2021-08-16 03:45:24 +0200

    fine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+
+import React from 'react'
+import { BrowserRouter as Router, Route ,Switch } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
+import Support from './pages/Support';
+import Messages from './pages/Messages';
+import Team from './pages/Team';
 
 function App() {
   return (
-    <div className="App">
-      <h1>prova</h1>
-    </div>
+      <>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/reports" component={Reports}/>
+          <Route path="/products" component={Products}/>
+          <Route path="/team" component={Team}/>
+          <Route path="/messages" component={Messages}/>
+          <Route path="/support" component={Support}/>
+        </Switch>
+      </Router>
+
+      </>
   );
 }
 
---
+++ src/pages/Team.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Team() {
+    return (
+        <div className="team">
+            Team
+        </div>
+    )
+}
+
+export default Team
