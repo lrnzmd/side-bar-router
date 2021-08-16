bd8cac72a2cc151780598dacc9a119ac9694d4b1
lrnzmd <lrnzmd@gmail.com>
2021-08-16 02:44:49 +0200

    inizio

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>prova</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import * as FaIcons from 'react-icons/fa';
+
+
+
+function Navbar() {
+    return (
+        <div>
+            <div className="navbar">
+                <Link to="#" className="menu-navbar">
+                    <FaIcons.FaBars/>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
